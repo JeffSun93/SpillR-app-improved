a4cfd60ea732aa3f28f8b6775a8b052dba518c1c
Jinson <jinson@Jinsons-MacBook-Air.local>
2026-03-10 15:37:05 +0000

    adds functionality to the searchbar

--- app/(tabs)/search.jsx
+++ app/(tabs)/search.jsx
@@ -1,10 +1,18 @@
 import { View, Text, StyleSheet } from "react-native";
+import { useState } from "react";
 import SearchBar from "../../components/searchBar";
 
 export default function Search() {
+  const [searchPhrase, setSearchPhrase] = useState("");
+  const [clicked, setClicked] = useState(false);
   return (
     <View style={styles.container}>
-      <SearchBar />
+      <SearchBar
+        searchPhrase={searchPhrase}
+        setSearchPhrase={setSearchPhrase}
+        clicked={clicked}
+        setClicked={setClicked}
+      />
       <View style={styles.results}>
         <Text>Search Page</Text>
       </View>
