70577072a198a1a1e981cbf9f369e4156530ba0a
yewen-jin <yewen.elsa.jin@gmail.com>
2026-03-17 15:02:16 +0000

    renamed the rest of components

---
+++ app/components/SearchBar.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import {
+    StyleSheet,
+    TextInput,
+    View,
+    Keyboard,
+    Button,
+    TouchableOpacity,
+} from "react-native";
+import { Feather, Entypo } from "@expo/vector-icons";
+
+const SearchBar = ({
+    clicked,
+    searchPhrase,
+    setSearchPhrase,
+    setClicked,
+    onSubmit,
+}) => {
+    return (
+        <View style={styles.container}>
+            <View
+                style={
+                    clicked
+                        ? styles.searchBar__clicked
+                        : styles.searchBar__unclicked
+                }
+            >
+                {/* Search Icon */}
+                <Feather name="search" size={20} color="black" />
+                {/* Input Field */}
+                <TextInput
+                    style={styles.input}
+                    placeholder="Search"
+                    value={searchPhrase}
+                    onChangeText={setSearchPhrase}
+                    onFocus={() => setClicked(true)}
+                    onSubmitEditing={onSubmit}
+                    returnKeyType="search"
+                />
+                {/* Cross Icon */}
+                {clicked && (
+                    <TouchableOpacity
+                        onPress={() => {
+                            setSearchPhrase("");
+                            setClicked(false);
+                            Keyboard.dismiss();
+                        }}
+                        onStartShouldSetResponder={() => true}
+                    >
+                        <Entypo name="cross" size={20} color="black" />
+                    </TouchableOpacity>
+                )}
+            </View>
+            {/* Cancel Button */}
+            {clicked && (
+                <Button
+                    title="Cancel"
+                    onPress={() => {
+                        Keyboard.dismiss();
+                        setClicked(false);
+                        setSearchPhrase("");
+                    }}
+                />
+            )}
+        </View>
+    );
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginVertical: 10,
+        width: "100%",
+    },
+    searchBar__unclicked: {
+        flex: 1,
+        padding: 10,
+        flexDirection: "row",
+        backgroundColor: "#e4e5e5",
+        borderRadius: 15,
+        alignItems: "center",
+    },
+    searchBar__clicked: {
+        flex: 1,
+        padding: 10,
+        flexDirection: "row",
+        backgroundColor: "#d9dbda",
+        borderRadius: 15,
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    input: {
+        fontSize: 18,
+        marginLeft: 10,
+        flex: 1,
+    },
+});
