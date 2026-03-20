c96af0079d822dfd579c8669cd6bfbe2ecfdd737
yewen-jin <yewen.elsa.jin@gmail.com>
2026-03-20 14:56:50 +0000

    fixed useContext not imported in Episode context

--- context/Episode.jsx
+++ context/Episode.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, createContext } from "react";
+import { useEffect, useState, createContext, useContext } from "react";
 
 export const EpisodeContext = createContext({});
 
