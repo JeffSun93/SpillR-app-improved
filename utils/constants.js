e59c9ac7a9b41afcf73861fc31f534e3915e06c0
Jeff Sun <143172312+JeffSun93@users.noreply.github.com>
2026-04-20 17:55:46 +0100

    Update backup API URL in constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,1 +1,1 @@
-export const url = process.env.EXPO_PUBLIC_API_URL || "https://spillr-be-improved-back.onrender.com";
+export const url = process.env.EXPO_PUBLIC_API_URL || "https://spillr-be-improved-backup.onrender.com";
