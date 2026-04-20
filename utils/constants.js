fd4df54df03ce6439b793cc3a920c3aa266f7609
Jeff Sun <143172312+JeffSun93@users.noreply.github.com>
2026-04-20 17:55:36 +0100

    Update API URL in constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,1 +1,1 @@
-export const url = process.env.EXPO_PUBLIC_API_URL || "https://spillr-be-improved.onrender.com";
+export const url = process.env.EXPO_PUBLIC_API_URL || "https://spillr-be-improved-back.onrender.com";
